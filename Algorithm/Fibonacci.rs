9ad395be6966235f623a19821db5fd34883973e4
Ahmet Göker <116346668+0xCD4@users.noreply.github.com>
2024-07-01 11:19:18 +0300

    Create Fibonacci.rs

---
+++ Algorithm/Fibonacci.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    pub fn fib(n: i32) -> i32 {
+        if n == 0 {
+            return 0;
+        }
+        if n == 1 {
+            return 1;
+        }
+        
+        let mut a = 0;
+        let mut b = 1;
+        for _ in 2..=n {
+            let temp = a + b;
+            a = b;
+            b = temp;
+        }
+        b
+    }
+}
